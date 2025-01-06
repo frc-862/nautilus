2a4875916384f43bdf43bffd56fa6b7c9133a2ed
doorio <jusnoord@gmail.com>
2025-01-06 14:52:52 -0500

    added and generated a swerve project, based on Triton (with estimated values for Nautilus). Triton drives on 2025 code.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,39 +4,49 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants.TunerConstants;
+import frc.robot.Constants.DrivetrainConstants.DriveRequests;
 import frc.robot.subsystems.PhotonVision;
+import frc.robot.subsystems.Swerve;
 import frc.thunder.LightningContainer;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 
 public class RobotContainer extends LightningContainer {
 
-    // public Swerve drivetrain;
+    public Swerve drivetrain;
     public PhotonVision vision;
+
+    private final XboxController driver = new XboxController(ControllerConstants.DRIVER_CONTROLLER);
 
     @Override
     protected void initializeSubsystems() {
-        // drivetrain = getDrivetrain();
+        drivetrain = TunerConstants.createDrivetrain();
         // vision = new PhotonVision();
+    }
+
+    @Override
+    protected void configureDefaultCommands() {
+        drivetrain.setDefaultCommand(drivetrain.applyRequest(DriveRequests.getDrive(()-> -driver.getLeftX(), ()-> -driver.getLeftY(), ()-> driver.getRightX())));
+    }
+
+
+    @Override
+    protected void configureButtonBindings() {
     }
     
     @Override
     protected void initializeNamedCommands() {
     }
 
-    @Override
-    protected void configureButtonBindings() {
-    }
 
-    @Override
-    protected void configureDefaultCommands() {
-    }
+    
 
     public Command getAutonomousCommand() {
         return null;
     }
-
-    // public Swerve getDrivetrain() {
-    //     return drivetrain == null ? TunerConstants.getDrivetrain() : drivetrain;
-    // }
-
 }

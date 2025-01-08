2d4a57f34dc7e5d5a828be52c50be49faaf0cfe6
Meh243 <120067923+Meh243@users.noreply.github.com>
2025-01-07 20:50:25 -0500

    [#15] - added a basic elevator subsystem

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,86 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.RobotMap;
+import frc.thunder.hardware.ThunderBird;
+
+public class Elevator extends SubsystemBase {
+
+    private ThunderBird leftMotor;
+    private ThunderBird rightMotor;
+
+    private final PositionVoltage elevatorPID = new PositionVoltage(0).withSlot(0);
+
+    /** Creates a new Lift. */
+    public Elevator() {
+        leftMotor = new ThunderBird(RobotMap.L_ELEVATOR, RobotMap.CANIVORE_CAN_NAME , ElevatorConstants.L_INVERTED,
+            ElevatorConstants.STATOR_CURRENT_LIMIT, ElevatorConstants.BRAKE_MODE);
+        rightMotor = new ThunderBird(RobotMap.R_ELEVATOR, RobotMap.CANIVORE_CAN_NAME, ElevatorConstants.R_INVERTED,
+            ElevatorConstants.STATOR_CURRENT_LIMIT, ElevatorConstants.BRAKE_MODE);
+
+        TalonFXConfiguration config = leftMotor.getConfig();
+        config.Slot0.GravityType = GravityTypeValue.Elevator_Static;
+        config.Slot0.kP = ElevatorConstants.MOTORS_KP;
+        config.Slot0.kI = ElevatorConstants.MOTORS_KI;
+        config.Slot0.kD = ElevatorConstants.MOTORS_KD;  
+        config.Slot0.kS = ElevatorConstants.MOTORS_KS;
+        config.Slot0.kV = ElevatorConstants.MOTORS_KV;
+        config.Slot0.kA = ElevatorConstants.MOTORS_KA;
+        config.Slot0.kG = ElevatorConstants.MOTORS_KG;
+
+        leftMotor.applyConfig(config);
+        rightMotor.setControl(new Follower(RobotMap.L_ELEVATOR, true));
+    }    
+
+    @Override
+    public void periodic() {}
+
+    /**
+     * sets the target position for the elevator
+     * @param target height value for the elevator
+     */
+    public void setPosition(double target) {
+        // we dont know exactly how far the elevator will move in one rotation, so no math is done here
+        leftMotor.setControl(elevatorPID.withPosition(target));
+    }
+
+    /**
+     * sets basic percentage power to the elevator motors
+     * @param power
+     */
+    public void setPower(double power) {
+        leftMotor.set(power);
+    }
+
+    /**
+     * stops the elevator motors
+     */
+    public void stop() {
+        setPower(0d);
+    }
+
+    /**
+     * gets the position of the elevator motors
+     * @return left motor position (which the right is synced to)
+     */
+    public double getPosition() {
+        return leftMotor.getPosition().getValueAsDouble();
+    }
+
+    /**
+     * gets the basic percentage power of the elevator motors
+     * @return left motor power (which the right is synced to)
+     */
+    public double getCurrentLeftPower() {
+        return leftMotor.get();
+    }
+}

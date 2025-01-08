8461eeee9097b56c6d8a18a68f3bd768e396982d
jfiaoejf <tjwellswork@gmail.com>
2025-01-07 19:24:10 -0500

    should work probably (maybe)

---
+++ src/main/java/frc/robot/commands/TestAutoAlign.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.DrivetrainConstants.DriveRequests;
+import frc.robot.subsystems.PhotonVision;
+import frc.robot.subsystems.Swerve;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class TestAutoAlign extends Command {
+  PhotonVision vision;
+  Swerve drivetrain;
+
+  double tx = 0;
+  PIDController controller = new PIDController(0.05d, 0, 0);
+  double calculatedSpeed;
+
+  public TestAutoAlign(PhotonVision vision, Swerve drivetrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.vision = vision;
+    this.drivetrain = drivetrain;
+
+    addRequirements(drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    controller.setSetpoint(0);
+    controller.setTolerance(0.2d);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (Math.abs(tx) > 0.2d) {
+      calculatedSpeed = controller.calculate(tx);
+      drivetrain.applyRequest(DriveRequests.getRobotCentric(() -> calculatedSpeed, () -> 0, () -> 0));
+    } else {
+      DataLogManager.log("Auto Align Finished");
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

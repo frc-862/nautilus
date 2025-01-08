29dd339a09d4b86d5d26a0ebd43506b3a583fbef
jfiaoejf <tjwellswork@gmail.com>
2025-01-07 20:40:45 -0500

    [#9] - linter should pass now

--- src/main/java/frc/robot/commands/StandinCommands.java
+++ src/main/java/frc/robot/commands/StandinCommands.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class StandinCommands {
-    /**
+    /*
      * move elevator
      * move wrist
      * move algae collector
@@ -15,7 +15,7 @@ public class StandinCommands {
      /**
       * Command will end after an approximation of how long it will take to move the elevator to a given position
       * @param position between 30 and 90 inches
-      * @return
+      * @return command
       */
     public static Command moveElevator(double position) {
         return new WaitCommand(position * 0.05); //assume ~20in/s

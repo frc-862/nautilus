c3fb4cc43a00d7733641b617431d76645b8402d2
jfiaoejf <tjwellswork@gmail.com>
2025-01-07 19:24:30 -0500

    [#12] - implemented shuffleboard logging

--- src/main/java/frc/robot/commands/TestAutoAlign.java
+++ src/main/java/frc/robot/commands/TestAutoAlign.java
@@ -4,12 +4,15 @@
 
 package frc.robot.commands;
 
+import org.opencv.features2d.FlannBasedMatcher;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.DrivetrainConstants.DriveRequests;
 import frc.robot.subsystems.PhotonVision;
 import frc.robot.subsystems.Swerve;
+import frc.thunder.shuffleboard.LightningShuffleboard;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class TestAutoAlign extends Command {
@@ -38,13 +41,21 @@ public class TestAutoAlign extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    tx = vision.getXBestTarget();
     if (Math.abs(tx) > 0.2d) {
       calculatedSpeed = controller.calculate(tx);
       drivetrain.applyRequest(DriveRequests.getRobotCentric(() -> calculatedSpeed, () -> 0, () -> 0));
     } else {
       DataLogManager.log("Auto Align Finished");
+
     }
+
+    LightningShuffleboard.setDouble("TestAutoAlign", "tX", tx);
+    LightningShuffleboard.setDouble("TestAutoAlign", "calculatedspeed", calculatedSpeed);
+    
   }
+
+
 
   // Called once the command ends or is interrupted.
   @Override
@@ -54,6 +65,6 @@ public class TestAutoAlign extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return Math.abs(tx) > 0.2d;
   }
 }

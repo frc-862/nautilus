80a59080bd8182c366045b4c2039a63758765bfe
jfiaoejf <tjwellswork@gmail.com>
2025-01-20 18:44:16 -0500

    oops

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -174,3 +175,5 @@ public class PhotonVision extends SubsystemBase {
         LightningShuffleboard.set("Vision", "Field_SIM", visionSim.getDebugField());
     }
 }
+=======
+>>>>>>> Stashed changes

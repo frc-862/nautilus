4892f20d7bdf2240bf78b335d58c013b4cc931ec
vilok1 <vgunti777@gmail.com>
2025-01-25 17:12:48 -0500

    [#62] clean code

--- src/main/java/frc/robot/commands/TestAutoAlign.java
+++ src/main/java/frc/robot/commands/TestAutoAlign.java
@@ -2,9 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.AutoAlignConstants;
 import frc.robot.Constants.DrivetrainConstants.DriveRequests;
@@ -16,32 +14,14 @@ public class TestAutoAlign extends Command {
     PhotonVision vision;
     Swerve drivetrain;
 
-    // get PID values from constants
-
     Transform3d diffFromTag;
-    // PIDController controllerRot = RobotBase.isReal() ? new PIDController(AutoAlignConstants.RotAutoAlignKp, 
-    //     AutoAlignConstants.RotAutoAlignKi, AutoAlignConstants.RotAutoAlignKd) : new PIDController(AutoAlignConstants.SimRotAutoAlignKp, 
-    //     AutoAlignConstants.SimRotAutoAlignKi, AutoAlignConstants.SimRotAutoAlignKd);
+  
+    PIDController controllerX = new PIDController(AutoAlignConstants.XAutoAlignKp, AutoAlignConstants.XAutoAlignKi, 
+        AutoAlignConstants.XAutoAlignKd);
 
-    // PIDController controllerX = RobotBase.isReal() ? new PIDController(AutoAlignConstants.XAutoAlignKp, AutoAlignConstants.XAutoAlignKi, 
-    //     AutoAlignConstants.XAutoAlignKd) : new PIDController(AutoAlignConstants.SimXAutoAlignKp, AutoAlignConstants.SimXAutoAlignKi,
-    //     AutoAlignConstants.SimXAutoAlignKd);
+    PIDController controllerY = new PIDController(AutoAlignConstants.YAutoAlignKp, AutoAlignConstants.YAutoAlignKi, 
+        AutoAlignConstants.YAutoAlignKd);
 
-    // PIDController controllerY = RobotBase.isReal() ? new PIDController(AutoAlignConstants.YAutoAlignKp, 
-    //     AutoAlignConstants.YAutoAlignKi, AutoAlignConstants.YAutoAlignKd) : new PIDController(AutoAlignConstants.SimYAutoAlignKp,
-    //     AutoAlignConstants.SimYAutoAlignKi, AutoAlignConstants.SimYAutoAlignKd);
-
-    PIDController controllerX = new PIDController(
-        LightningShuffleboard.getDouble("TestAutoAlign", "X Kp", 0), 
-        LightningShuffleboard.getDouble("TestAutoAlign", "X Ki", 0), 
-        LightningShuffleboard.getDouble("TestAutoAlign", "X Kd", 0));
-
-    PIDController controllerY = new PIDController(
-        LightningShuffleboard.getDouble("TestAutoAlign", "Y Kp", 0), 
-        LightningShuffleboard.getDouble("TestAutoAlign", "Y Ki", 0), 
-        LightningShuffleboard.getDouble("TestAutoAlign", "Y Kd", 0));
-
-    double dr_dt;
     double dx_dt;
     double dy_dt;
 
@@ -74,13 +54,6 @@ public class TestAutoAlign extends Command {
             LightningShuffleboard.getDouble("TestAutoAlign", "Y Ki", 0), 
             LightningShuffleboard.getDouble("TestAutoAlign", "Y Kd", 0));
 
-        
-        dx_dt = 0;
-        dy_dt = 0;
-
-        pitch = vision.getPitch();
-        yaw = vision.getYaw();
-
         controllerX.setSetpoint(0);
         controllerX.setTolerance(AutoAlignConstants.AutoAlignTolerance);
 
@@ -92,33 +65,28 @@ public class TestAutoAlign extends Command {
     @Override
     public void execute() {
 
-        // zero velocity values
-        dx_dt = 0;
-        dy_dt = 0;
+        // update pitch and yaw values
 
         pitch = vision.getPitch();
         yaw = vision.getYaw();
 
         if(!vision.hasTarget()){
+
+            System.out.println("Error: Cannot See April Tag");
+
             drivetrain.setControl(DriveRequests.getRobotCentric(0, 0, 0));
             return;
         }
 
-        // if the difference is greater than the tolerance, calculate the new velocity values
+        // use pitch and yaw to calculate velocity values
 
-        if (/*Math.abs(diffFromTag.getRotation().getZ()) > AutoAlignConstants.AutoAlignTolerance || 
-            Math.abs(diffFromTag.getTranslation().getX()) > AutoAlignConstants.AutoAlignTolerance 
-            || Math.abs(diffFromTag.getTranslation().getY()) > AutoAlignConstants.AutoAlignTolerance*/ true) {
-
-                dx_dt = controllerX.calculate(yaw);
-                dy_dt = -controllerY.calculate(pitch); // X and Y are intentionally flipped here
-        }
+        dx_dt = controllerX.calculate(yaw);
+        dy_dt = -controllerY.calculate(pitch);
+        
 
         if (!DriverStation.isFMSAttached()){
-            LightningShuffleboard.setDouble("TestAutoAlign", "X diffrence", 
-                yaw);
-            LightningShuffleboard.setDouble("TestAutoAlign", "Y diffrence", 
-                pitch);
+            LightningShuffleboard.setDouble("TestAutoAlign", "X diffrence", yaw);
+            LightningShuffleboard.setDouble("TestAutoAlign", "Y diffrence", pitch);
 
             LightningShuffleboard.setDouble("TestAutoAlign", "X speed", dx_dt);
             LightningShuffleboard.setDouble("TestAutoAlign", "Y speed", dy_dt);
@@ -137,11 +105,7 @@ public class TestAutoAlign extends Command {
 
     @Override
     public boolean isFinished() {
-
-        return false;
-
-        // return Math.abs(diffFromTag.getRotation().getZ()) < AutoAlignConstants.AutoAlignTolerance && 
-        //     Math.abs(diffFromTag.getTranslation().getX()) < AutoAlignConstants.AutoAlignTolerance && 
-        //     Math.abs(diffFromTag.getTranslation().getY()) < AutoAlignConstants.AutoAlignTolerance;
+        return Math.abs(vision.getPitch()) < AutoAlignConstants.AutoAlignTolerance && 
+            Math.abs(vision.getYaw()) < AutoAlignConstants.AutoAlignTolerance;
     }
 }

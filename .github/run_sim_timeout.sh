6a7a62d8d9c8399894cf93029bfddde3981cba5d
Patrick Hurley <phurley@invoca.com>
2025-02-08 13:56:44 -0500

    Fix run syntax

--- .github/run_sim_timeout.sh
+++ .github/run_sim_timeout.sh
@@ -13,9 +13,11 @@ run_simulator() {
     timeout $TIMEOUT ./gradlew simulateJava > $LOG_FILE 2>&1
     # timeout $TIMEOUT ./gradlew simulateJava   -Dorg.gradle.java.home="C:\Users\Public\wpilib\2025\jdk" > $LOG_FILE 2>&1
     EXIT_CODE=$?
+    echo Simulation exited with code $EXIT_CODE
 
     # Check the exit code of the timeout command
     if [ $EXIT_CODE -eq 124 ]; then
+        echo Simulation timed out
         if grep -q "********** Robot program starting **********" "$LOG_FILE"; then
             cat $LOG_FILE
             echo -e "\033[1;32mSimulation ran for \033[1;34m${TIMEOUT}\033[1;32m seconds with no errors.\033[0m"
@@ -29,6 +31,7 @@ run_simulator() {
     fi
 
     # Check the log file for runtime errors
+    echo Simulation did not time out
     if grep -q "Error" "$LOG_FILE"; then
         cat $LOG_FILE
         echo -e "\033[1;31mRuntime error(s) detected in the simulation!\033[0m"
@@ -42,12 +45,14 @@ run_simulator() {
     return 2
 }
 
-RUN_SUCCESSFULNESS=$(run_simulator "40")
+run_simulator 40
+RUN_SUCCESSFULNESS=$?
 
-if [[ $RUN_SUCCESSFULNESS -eq 0 ]]; then
+if [[ $RUN_SUCCESSFULNESS -eq 0]]; then
     exit 0
 elif [[ $RUN_SUCCESSFULNESS -eq 1 ]]; then
-    RUN_SUCCESSFULNESS=$(run_simulator "120")
+    run_simulator 120
+    RUN_SUCCESSFULNESS=$?
 elif [[ $RUN_SUCCESSFULNESS -eq 2 ]]; then
     exit 2
 elif [[ $RUN_SUCCESSFULNESS -eq 3 ]]; then
@@ -63,73 +68,3 @@ elif [[ $RUN_SUCCESSFULNESS -eq 2 ]]; then
 elif [[ $RUN_SUCCESSFULNESS -eq 3 ]]; then
     exit 3
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# #!/bin/bash
-
-# # Log file to capture simulator output
-# LOG_FILE="simulate.log"
-
-# run_simulator() {
-#     # Timeout duration (in seconds)
-#     TIMEOUT=$1
-
-#     echo -e "\033[0;36mStarted Gradle Simulation with a timeout of \033[0;34m${TIMEOUT}\033[0;36m seconds...\n\033[0;37mNote: There is no logging while the simulation is running. The log will be dumped afterward.\033[0m"
-
-#     # Run the simulator with timeout and capture output
-#     timeout $TIMEOUT ./gradlew simulateJava > $LOG_FILE 2>&1
-#     # timeout $TIMEOUT ./gradlew simulateJava   -Dorg.gradle.java.home="C:\Users\Public\wpilib\2025\jdk" > $LOG_FILE 2>&1
-#     EXIT_CODE=$?
-
-#     # Check if the simulation ran successfully in time
-#     if [ $EXIT_CODE -eq 124 ]; then
-#         if grep -q "********** Robot program starting **********" "$LOG_FILE"; then
-#             cat $LOG_FILE
-#             echo -e "\033[1;32mSimulation ran for \033[1;34m${TIMEOUT}\033[1;32m seconds with no errors.\033[0m"
-#             return 0
-#         fi
-#         cat $LOG_FILE
-#         echo -e "\033[1;33mSimulation did not start successfully. (Is the timeout too short?)\033[0m"
-#         return 1
-#     fi
-
-#     # Check the log file for runtime errors
-#     if grep -q "Error" "$LOG_FILE"; then
-#         cat $LOG_FILE
-#         echo -e "\033[1;31mRuntime error(s) detected in the simulation!\033[0m"
-#         return 1
-#     fi
-
-#     # Check if the simulation exited immediately
-#     cat $LOG_FILE
-#     echo -e "\033[1;33mSimulation exited immediately. (Is gradlew working?)\033[0m"
-#     return 1
-# }
-
-# # first run the simulation for 40 seconds
-# if [[ $(run_simulator 40) -eq 0 ]]; then
-#     exit 0
-# fi
-# echo "failed first run"
-
-# # if timeout, run the simulation for 120 seconds
-# if [[ $(run_simulator 120) -eq 0 ]]; then
-#     exit 0
-# fi
-# echo "failed second run"
-
-# # if timeout or another error, exit with error
-# exit 1

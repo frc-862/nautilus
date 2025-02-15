1b5d7a16a5c6c1b9529eb231f4d9976d3792b559
Meh243 <120067923+Meh243@users.noreply.github.com>
2025-02-14 21:18:56 -0500

    [#208] - eh

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -156,10 +156,10 @@ public class RobotContainer extends LightningContainer {
             .onTrue(new InstantCommand(() -> drivetrain.setSlowMode(true)))
             .onFalse(new InstantCommand(() -> drivetrain.setSlowMode(false)));
         
-        // sets slow mode if the elevator is above L3
-        new Trigger(() -> ((rod.getTargetState() == RodStates.L3) || (rod.getTargetState() == RodStates.L4)))
-            .onTrue(new InstantCommand(() -> drivetrain.setSlowMode(true)))
-            .onFalse(new InstantCommand(() -> drivetrain.setSlowMode(false)));
+        // // sets slow mode if the elevator is above L3
+        // new Trigger(() -> ((rod.getTargetState() == RodStates.L3) || (rod.getTargetState() == RodStates.L4)))
+        //     .onTrue(new InstantCommand(() -> drivetrain.setSlowMode(true)))
+        //     .onFalse(new InstantCommand(() -> drivetrain.setSlowMode(false)));
 
         // drivetrain brake
         new Trigger(driver::getXButton).whileTrue(drivetrain.applyRequest(DriveRequests.getBrake()));

3509897897be2e51d49c6d34481ccbb97e73279f
vilok1 <vgunti777@gmail.com>
2025-02-24 20:37:51 -0500

    [#251] actual progress

--- src/main/java/frc/robot/subsystems/ReefDisplay.java
+++ src/main/java/frc/robot/subsystems/ReefDisplay.java
@@ -7,11 +7,75 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ReefDisplay extends SubsystemBase {
-  /** Creates a new ReefDisplay. */
-  public ReefDisplay() {}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    CoralCollector collector;
+    FishingRod rod;
+    Swerve drivetrain;
+    Pose2d robotPose;
+
+    Tuple<Camera, Integer> targetReefArm;
+    int tagNum;
+
+    Boolean[][] reef = new Boolean[][] {new Booean[] {false, false, false, false, false, false, false, false, false, false, false, false},
+        new Booean[] {false, false, false, false, false, false, false, false, false, false, false, false},
+        new Booean[] {false, false, false, false, false, false, false, false, false, false, false, false}}; // iner = level; outer = tag
+
+    public ReefDisplay(CoralCollector coralCollector, FishingRod rod, Swerve drivetrain) {
+        this.collector = coralCollector;
+        this.rod = rod;
+        this.drivetrain = drivetrain;
+    }
+
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    }
+
+    public void updateReef(){
+
+        if (targetReefArm == null || tagNum == null){
+            return;
+        }
+
+        if (collector.getVelocity() > 0){
+            switch(rod.getState()){
+                case L2:
+                  if (tagNum == 18 || tagNum == 7){
+                      if(targetReefArm.getFirst() == Camera.LEFT){
+                          reef[0][0] = true;
+                      }
+
+                      if(targetReefArm.getFirst() == Camera.RIGHT){
+                          reef[0][1] = true;
+                      }
+                  }
+                  break;
+
+                case L3:
+                    if (tagNum == 17 || tagNum == 8){
+                        if(targetReefArm.getFirst() == Camera.RIGHT){
+                            reef[1][0] = true;
+                        }
+
+                        if(targetReefArm.getFirst() == Camera.LEFT){
+                            reef[1][1] = true;
+                        }
+                    }
+                    break;
+
+                case L4:
+                    break;
+            }
+        }
+    }
+
+    public void updateScorePose(Tuple<Camera, Integer> targetReefArm){
+        this.targetReefArm = targetReefArm;
+        tagNum = targetReefArm.getSecond();
+    }
+
+    public void publish(){
+        NetworkTableInstance.getDefault().getTable("Shuffleboard").getSubTable("ReefDisplay").getEntry("L1").setBooleanArray(reef[0]);
+    }
+
 }

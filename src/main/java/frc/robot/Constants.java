6e740778e249f6ae480fee2153bae60aa682d770
WindowsVistaisCool <kyle@quackings.com>
2025-03-15 08:43:04 -0400

    goooooodddd morninnggg USAAAAAA

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -729,8 +729,8 @@ public class Constants {
             // When using closed-loop control, the drive motor uses the control
             // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
             private static final Slot0Configs driveGains = new Slot0Configs()
-            .withKP(0.6507).withKI(0).withKD(0)
-            .withKS(0.33986).withKV(0.12318).withKA(0.0059707);
+                .withKP(0.6507).withKI(0).withKD(0)
+                .withKS(0.33986).withKV(0.12318).withKA(0.0059707);
 
             // The closed-loop output type to use for the steer motors;
             // This affects the PID/FF gains for the steer motors

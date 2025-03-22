e03030aa7d267913e8037d9fc3805be83ac9d63e
WindowsVistaisCool <kyle@quackings.com>
2025-03-22 07:46:32 -0400

    change wrist setpoint and add back different autoalign gains

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -231,7 +231,7 @@ public class Constants {
                 put(RodStates.L1, 6d);
                 put(RodStates.L2, -30d);
                 put(RodStates.L3, -36d);
-                put(RodStates.L4, -48d);
+                put(RodStates.L4, -45d); // -48
                 put(RodStates.LOW, -29d);
                 put(RodStates.HIGH, -29d);
                 put(RodStates.SOURCE, 42d);
--- src/main/java/frc/robot/commands/auton/AutonAutoAlign.java
+++ src/main/java/frc/robot/commands/auton/AutonAutoAlign.java
@@ -141,16 +141,16 @@ public class AutonAutoAlign extends Command {
             }
         }
 
-        // if (DriverStation.isAutonomous()) {
-        //     xPID.setPID(AutoAlignConstants.AUTON_DRIVE_P, AutoAlignConstants.AUTON_DRIVE_I, AutoAlignConstants.AUTON_DRIVE_D);
-        //     yPID.setPID(xPID.getP(), xPID.getI(), xPID.getD());
-        //     driveKS = AutoAlignConstants.AUTON_DRIVE_KS;
-        // } else {
-        //     xPID.setPID(AutoAlignConstants.TELE_DRIVE_P,
-        //         AutoAlignConstants.TELE_DRIVE_I, AutoAlignConstants.TELE_DRIVE_D);
-        //     yPID.setPID(xPID.getP(), xPID.getI(), xPID.getD());
-        //     driveKS = AutoAlignConstants.TELE_DRIVE_KS;
-        // }
+        if (DriverStation.isAutonomous()) {
+            xPID.setPID(AutoAlignConstants.AUTON_DRIVE_P, AutoAlignConstants.AUTON_DRIVE_I, AutoAlignConstants.AUTON_DRIVE_D);
+            yPID.setPID(xPID.getP(), xPID.getI(), xPID.getD());
+            driveKS = AutoAlignConstants.AUTON_DRIVE_KS;
+        } else {
+            xPID.setPID(AutoAlignConstants.TELE_DRIVE_P,
+                AutoAlignConstants.TELE_DRIVE_I, AutoAlignConstants.TELE_DRIVE_D);
+            yPID.setPID(xPID.getP(), xPID.getI(), xPID.getD());
+            driveKS = AutoAlignConstants.TELE_DRIVE_KS;
+        }
 
         xPID.setTolerance(driveTolerance);
 
